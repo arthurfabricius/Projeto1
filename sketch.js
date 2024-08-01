ce4cf58d6e32fa88690d666cb1224f7b7c7a0218
arthurfabricius <00001149831716sp@al.educacao.sp.gov.br>
2024-08-01 18:38:40 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(1000, 800); 
+  background("#3AB1AC")
+}
+
+function draw(){
+  
+  fill("purple")
+
+  stroke("#17E920");
+  if(mouseIsPressed)
+  
+  
+  
+  
+ rect(mouseX,mouseY,30,50);
+}
